f1bdea0a57cbd59d98934ecc78e3337fec1dca09
mauz315 <mezaponze@gmail.com>
2018-04-27 20:32:13 -0500

    starting point with pandas

--- graph1.py
+++ graph1.py
@@ -2,13 +2,12 @@
 # libraries and data
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('Graph1.csv',';')
+df = pd.read_csv('Graph1.csv', ';')
 
-print(df)
+graph1 = plt.plot('Date', 'USD', data=df)
 
-plt.plot('Date', 'USD', data=df)
 plt.show()
-
