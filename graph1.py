75a943e3ced7b079cdc566ad9aeab6371a29df8c
mauz315 <mezaponze@gmail.com>
2018-04-27 21:52:06 -0500

    graph formatting

--- graph1.py
+++ graph1.py
@@ -5,9 +5,19 @@ import numpy as np
 # import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-df = pd.read_csv('Graph1.csv', ';')
+df = pd.read_csv('Graph1.csv', ';', parse_dates=True)
+df.reset_index(drop=True, inplace=True)
+df.set_index("Date", inplace=True)
 
-graph1 = plt.plot('Date', 'USD', data=df)
+print(df)
 
-plt.show()
+fig, ax = plt.subplots()
+ax.plot(df)
+ax.set_xticks(df.index)
+ax.xaxis_date()
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter("%Y-%m"))
+
+fig.show()
